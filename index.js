1e1821bd1a9e4ebcc7aeb8b3694cec63d04e7ea9
Viktoria <doduchenko29@gmail.com>
2021-06-24 20:57:05 +0300

    add

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks';
 
-
 export function getTasksList() {
-  return fetch(baseUrl).then(response => response.json())
+  return fetch(baseUrl).then((response) => response.json());
 }
 
 export function getTaskById(taskId) {
-  return fetch('https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks')
-    .then((response) => response.json())
-    .then((resArr) => resArr.find((el) => el.id === taskId));
+  return fetch(
+    `https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks/${taskId}`
+  ).then((response) => response.json());
 }
-
 
 /*getTasksList().then((tasksList) => {
   console.log(tasksList); 
