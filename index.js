38aa06b983f4cb5ac4ba8c6a3ac1a67005668825
Viktoria <doduchenko29@gmail.com>
2021-06-24 20:52:05 +0300

    add

---
+++ index.js
@@ -0,0 +1,21 @@
+const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks';
+
+
+export function getTasksList() {
+  return fetch(baseUrl).then(response => response.json())
+}
+
+export function getTaskById(taskId) {
+  return fetch('https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks')
+    .then((response) => response.json())
+    .then((responseArr) => responseArr.find((el) => el.id === taskId));
+}
+
+
+/*getTasksList().then((tasksList) => {
+  console.log(tasksList); 
+});
+
+getTaskById('2').then((taskData) => {
+  console.log(taskData); 
+});*/
